7933c02d4b49ed63e9a4a6727fe49f62d47a6264
Joseph W Becher <jwbecher@drazisil.com>
2019-02-13 18:23:46 -0500

    clearer code

--- discourse.py
+++ discourse.py
@@ -12,76 +12,45 @@ DISCOURSE_USERNAME = os.getenv("DISCOURSE_USERNAME")
 DISCOURSE_API_TOKEN = os.getenv("DISCOURSE_API_TOKEN")
 
 
-def generateDiscourseUrl(base_url, report_name, start_date, end_date, username, api_token):
+def days_ago(number_of_days):
+    from datetime import datetime, timedelta
+    return (datetime.today() - timedelta(days=number_of_days)).date().isoformat()
+
+
+def generate_discourse_report_url(base_url, report_name, start_date, end_date, username, api_token):
     """Return the Discourse API endpoint url with *base_url* and *timestamp* attached"""
     return "{}/admin/reports/{}.json?end_date={}&start_date={}&api_key={}&api_username={}".format(base_url, report_name, end_date, start_date, api_token, username)
 
 
-def fetchAcceptedSolutions(base_url, start_date, end_date, username, token):
-    report_name = 'accepted_solutions'
+def fetch_report_from_discourse(report_name, headers, base_url, start_date, end_date, username, token):
     print("Fetching {} from {} to {}...".format(
         report_name, start_date, end_date))
-    url = generateDiscourseUrl(
+    url = generate_discourse_report_url(
         base_url, report_name, start_date, end_date, username, token)
     response = requests.get(url)
     report_data = json.loads(response.text)['report']
 
-    with open('data/discourse/discourse_accepted_solutions.csv', 'w') as output:
+    with open('data/discourse/discourse_{}.csv'.format(report_name), 'w') as output:
         csvwriter = csv.writer(output)
         csvwriter.writerow(
-            ['date', 'count'])
-        for item in report_data['data']:
-            csvwriter.writerow([item['x'], item['y']])
-
-
-def fetchPosts(base_url, start_date, end_date, username, token):
-    report_name = 'posts'
-    print("Fetching {} from {} to {}...".format(
-        report_name, start_date, end_date))
-    url = generateDiscourseUrl(
-        base_url, report_name, start_date, end_date, username, token)
-    response = requests.get(url)
-    report_data = json.loads(response.text)['report']
-
-    with open('data/discourse/discourse_posts.csv', 'w') as output:
-        csvwriter = csv.writer(output)
-        csvwriter.writerow(
-            ['date', 'count'])
-        for item in report_data['data']:
-            csvwriter.writerow([item['x'], item['y']])
-
-
-def fetchTimeToFirstResponse(base_url, start_date, end_date, username, token):
-    report_name = 'time_to_first_response'
-    print("Fetching {} from {} to {}...".format(
-        report_name, start_date, end_date))
-    url = generateDiscourseUrl(
-        base_url, report_name, start_date, end_date, username, token)
-    response = requests.get(url)
-    report_data = json.loads(response.text)['report']
-
-    with open('data/discourse/discourse_time_to_first_response.csv', 'w') as output:
-        csvwriter = csv.writer(output)
-        csvwriter.writerow(
-            ['date', 'hours_to_first_response'])
+            headers)
         for item in report_data['data']:
             csvwriter.writerow([item['x'], item['y']])
 
 
 def main():
-    from datetime import datetime, timedelta
 
-    end_date = (datetime.today() - timedelta(days=1)).date().isoformat()
-    start_date = (datetime.today() - timedelta(days=31)).date().isoformat()
+    end_date = days_ago(1)
+    start_date = days_ago(31)
 
-    fetchAcceptedSolutions(DISCOURSE_URL, start_date,
-                           end_date, DISCOURSE_USERNAME, DISCOURSE_API_TOKEN)
+    fetch_report_from_discourse('posts', ['date', 'count'], DISCOURSE_URL, start_date,
+                                end_date, DISCOURSE_USERNAME, DISCOURSE_API_TOKEN)
 
-    fetchPosts(DISCOURSE_URL, start_date,
-               end_date, DISCOURSE_USERNAME, DISCOURSE_API_TOKEN)
+    fetch_report_from_discourse('accepted_solutions', ['date', 'count'], DISCOURSE_URL, start_date,
+                                end_date, DISCOURSE_USERNAME, DISCOURSE_API_TOKEN)
 
-    fetchTimeToFirstResponse(DISCOURSE_URL, start_date,
-                             end_date, DISCOURSE_USERNAME, DISCOURSE_API_TOKEN)
+    fetch_report_from_discourse('time_to_first_response', ['date', 'hours_to_first_response'], DISCOURSE_URL, start_date,
+                                end_date, DISCOURSE_USERNAME, DISCOURSE_API_TOKEN)
 
 
 if __name__ == '__main__':
